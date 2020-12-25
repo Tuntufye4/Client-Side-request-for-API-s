2805336101c8a0fe5ffc45f6e59ac2c30f7353ea
Tuntufye Mwanyongo <tuntumwanyongo@gmail.com>
2020-12-25 13:02:05 +0200

    Add files via upload

---
+++ client_request.php
@@ -0,0 +1,12 @@
+$headers = array('Accept' => 'application/json');
+$options = array('auth' => array('user', 'pass'));
+$request = Requests::get('https://api.github.com/gists', $headers, $options);
+
+var_dump($request->status_code);
+// int(200)
+
+var_dump($request->headers['content-type']);
+// string(31) "application/json; charset=utf-8"
+
+var_dump($request->body);
+// string(26891) "[...]"
