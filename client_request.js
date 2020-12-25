c9df39c38ca8e60f55714e1c2d43fbc461870753
Tuntufye Mwanyongo <tuntumwanyongo@gmail.com>
2020-12-25 12:37:56 +0200

    Add files via upload

---
+++ client_request.js
@@ -0,0 +1,16 @@
+var xmlhttp;
+if (window.XMLHttpRequest){
+  xmlhttp=new XMLHttpRequest();
+}
+else{
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.onreadystatechange=function(){
+   if (xmlhttp.readyState==4 && xmlhttp.status==200){
+       ...handle response....
+ 
+ }
+}
+
+xmlhttp.open("GET", "a URL here", true);
+xmlhttp.send();
